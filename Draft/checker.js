323aa47347359654297655a01c80569c0e5706ce
Sai Oku <127640676+saioku@users.noreply.github.com>
2023-10-08 10:58:14 -0400

    Add files via upload

---
+++ Draft/checker.js
@@ -0,0 +1,31 @@
+function checker(guess, goal, dictionary) {
+
+    //const row = state.currentRow;
+
+    for (let i = 0; i < dictionary.length; i++){
+
+	if (dictionary[i] == guess)
+	    continue;
+	else
+	    return 'wrong'; 
+	    
+    for (let i=0; i<5; i++){
+
+	//const box = document.getElementById('box${row}${i}');
+
+	if (guess[i] == goal[i])
+	    //box.classList.add('green');
+	    return 'green'; 
+	    
+	else if (goal.includes(guess[i]))
+	    //box.classList.add('yellow');
+	    return 'yellow'; 
+	
+	else 
+	    //box.classList.add('grey');
+	    return 'grey';
+	    
+        }
+
+    }
+} 
