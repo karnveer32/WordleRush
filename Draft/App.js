0408652c5ac6153299a1733d83d43ab1b15c9d25
Karnveer Singh <81723756+karnveer32@users.noreply.github.com>
2023-10-11 16:12:33 -0400

    Update App.js

--- Draft/App.js
+++ Draft/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 const App = () => {
     const [words, setWords] = useState([]); 
     const [generatedWords, setGeneratedWords] = useState([]);
     const [currentWord, setCurrentWord] = useState(null);
+    const [currentGuess, setCurrentGuess] = useState('') 
 
     useEffect(() => {
       async function loadWords() {
@@ -45,20 +46,71 @@ const App = () => {
           console.log(word + " was already generated.");
       }
   };
+  function checkGuess(currrentGuess) //placeholder (Sai)
+  {
+    if (currentGuess === currentWord) {
+      console.log("you guessed the word")
+      //color-code
+      //generator()
+    }
+    else {
+      setCurrentGuess(currentGuess => '')
+      console.log("did not guess the word")
+      //color-code
+    }
+  }
+
+
+
+  function validGuess(currentGuess) //placeholder (Eric) //reset currentGuess if not valid
+  {
+    if (currentGuess.length === 5) {
+      checkGuess(currentGuess)
+      //setGuessses
+    }
+
+  }
+
+  function isAlpha(str) {
+    return /^[a-zA-Z]$/.test(str);
+  }
+
+
+  function userInput({ key }) {
+    if (isAlpha(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess(currentGuess => currentGuess + key)
+
+      }
+
+    }
+    else if (key === 'Backspace') {
+      setCurrentGuess(currentGuess => currentGuess.substring(0, currentGuess.length - 1))
+    }
+    else if (key === "Enter") {
+      validGuess(currentGuess)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', userInput)
+
+    return () => {
+      window.removeEventListener('keydown', userInput)
+    };
+  }, [userInput])
 
   return (
-      <div>
-        <h1>Word Generator</h1>
-        <button onClick={wordGeneration}>Generate Word</button>
-        {currentWord && <p> Generated Word: {currentWord} </p>}
-        <ul>
-          {generatedWords.map((word, index) => (
-            <li key={index}>{word}</li>
-          ))}
-        </ul>
-      </div>
-    );
+    <div>
+      <h1>WordleRush</h1>
+      <h1>Word Generator</h1>
+      <button onClick={wordGeneration}>Generate Word</button>
+      {currentWord && <p> Generated Word: {currentWord}  <br></br> current guess: {currentGuess} </p>}
+      <ul></ul>
 
-};
+
+    </div>
+  );
+}
 
 export default App;
